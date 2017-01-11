f976a0186863f69689c7a11543e3531b30b01402
ValeryMur <valery.muravska@gmail.com>
2017-01-11 19:06:10 +0200

    http

--- src/test/java/lesson19/TestOkHttp.java
+++ src/test/java/lesson19/TestOkHttp.java
@@ -29,8 +29,22 @@ public class TestOkHttp {
         @Test
         public void testPost() throws IOException{
             System.out.println(run1("http://httpbin.org/post"));
-
         }
+
+    @Test //чего не выводится инфо внутри скобок?
+    public void testCooki() throws IOException{
+        System.out.println(run2("http://httpbin.org/cookies"));
+        String resp = run(url);
+        GetResponse getResp = gson.fromJson(resp, GetResponse.class);
+        System.out.println(getResp);
+    }
+    @Test //чего не выводится инфо внутри скобок?
+    public void testCookiesSet() throws IOException{
+        System.out.println(run3("http://httpbin.org/cookies"));
+//        String resp = run(url);
+//        GetResponse getResp = gson.fromJson(resp, GetResponse.class);
+//        System.out.println(getResp);
+    }
 
     String run1(String url) throws IOException {
         OkHttpClient client = new OkHttpClient();
@@ -42,6 +56,13 @@ public class TestOkHttp {
                 .url(url)
                 .addHeader("Jack", "Sparow")
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36")
+                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                .header("Accept-Encoding", "gzip, deflate, sdch")
+                .header("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4")
+                .header("Cookie", "k2=v2; k1=v1; _gat=1; _ga=GA1.2.764998044.1482737465")
+                .header("Host", "httpbin.org")
+                .header("Referer", "http://httpbin.org/")
+                .header("Upgrade-Insecure-Requests", "1")
                 .post(requestBody)
                 .build();
         Response response = client.newCall(request).execute();
@@ -54,9 +75,38 @@ public class TestOkHttp {
                 .url(url)
                 .addHeader("Jack","Sparow")
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36")
+                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                .header("Accept-Encoding", "gzip, deflate, sdch")
+                .header("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4")
+                .header("Cookie", "k2=v2; k1=v1; _gat=1; _ga=GA1.2.764998044.1482737465")
+                .header("Host", "httpbin.org")
+                .header("Referer", "http://httpbin.org/")
+                .header("Upgrade-Insecure-Requests", "1")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+    String run2 (String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("l","l")
+                .header("_ga", "GA1.2.764998044.1482737465")
+                .header("_gat", "1")
                 .build();
         Response response = client.newCall(request).execute();
         return response.body().string();
     }
 
+    String run3 (String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .header("_ga", "GA1.2.764998044.1482737465")
+               .header("k1", "v1")
+               .header("k2", "v2")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
 }
--- src/test/java/lesson19/models/GetResponse.java
+++ src/test/java/lesson19/models/GetResponse.java
@@ -10,6 +10,7 @@ public class GetResponse {
     public String url;
     public Headers headers;
     public Args args;
+    public Cookies cookies;
 
     @Override
     public String toString() {
@@ -28,13 +29,51 @@ public class GetResponse {
     public class Headers {
         @SerializedName("User-Agent")
         public String userAgent;
+        public String Accept;
+        @SerializedName("Accept-Encoding")
+        public String acceptEncoding;
+        @SerializedName("Accept-Language")
+        public String acceptLanguage;
+        public String Cookie;
+        public String Host;
+        public String Referer;
+        @SerializedName("Upgrade-Insecure-Requests")
+        public String upgradeInsecureRequests;
 
         @Override
         public String toString() {
             return "Headers{" +
                     "userAgent='" + userAgent + '\'' +
+                    ", Accept='" + Accept + '\'' +
+                    ", acceptEncoding='" + acceptEncoding + '\'' +
+                    ", acceptLanguage='" + acceptLanguage + '\'' +
+                    ", Cookie='" + Cookie + '\'' +
+                    ", Host='" + Host + '\'' +
+                    ", Refere='" + Referer + '\'' +
+                    ", upgradeInsecureRequests='" + upgradeInsecureRequests + '\'' +
                     '}';
         }
     }
+
+        public class Cookies {
+            @SerializedName("_ga")
+            public String ga;
+            @SerializedName("_gat")
+            public String gat;
+            public String k1;
+            public String k2;
+
+            @Override
+            public String toString() {
+                return "Cookies{" +
+                        "ga='" + ga + '\'' +
+                        ", gat='" + gat + '\'' +
+                        ", k1='" + k1 + '\'' +
+                        ", k2='" + k2 + '\'' +
+                        '}';
+            }
+        }
+
+
 
 }
